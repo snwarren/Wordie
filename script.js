6817be739be3e33cdaea6aa7ae3a82dd195cca2d
snwarren <57119421+snwarren@users.noreply.github.com>
2022-01-30 17:11:37 -0500

    v5_FIRST FULLY FUNCTIONAL!
    Updated CSS, converted to flexbox.  Moved uppercase to top of function. Fixed classnames and appending to gameboard.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 //random keyword generator
-let wordDict = ['apple','heron','barge','trees','mouse','count']
-let rand = Math.floor(Math.random()*4);
+let wordDict = ['APPLE','HERON','BARGE','TREES','MOUSE','COUNT']
+let rand = Math.floor(Math.random()*6);
 let keyWord = wordDict[rand];
 //creates array of keyword letters to compare to guess
 let keyWordParse = keyWord.split("");
@@ -14,29 +14,37 @@ console.log(keyWord);
 function checkWord (){
     //stores user input
     let wordInput = document.getElementById("word").value;
+    const gameBoard = document.getElementById("gameboard");
 
+    //creates new answerbox each time a word is submitted
+    const answerBox = document.createElement("div");
+    answerBox.className = "answerBox"
+
+    wordInput = wordInput.toUpperCase();
     //creates array of input word letters
-    inputParse = wordInput.split("");
+    let inputParse = wordInput.split("");
 
     //compares input and keyword
     for (let i=0; i<inputParse.length; i++) {
-        let guessIndex = "guessLetter" + i;
-        let letter = document.getElementById(guessIndex);
+
+        var newItem = document.createElement("span");
+        newItem.className = "letter";
         //assigns color
         
         if (inputParse[i] === keyWordParse[i]){
-            letter.className="right"
-        }
-        else {
+            newItem.className += " right";
+        } else {
 
             for (let j=0; j<keyWordParse.length; j++){
                 if (inputParse[i] === keyWordParse[j]) {
-                    letter.className="sorta"
+                    newItem.className += " sorta";
                 }
             }
         }
+        newItem.innerHTML = inputParse[i];
+        gameBoard.appendChild(answerBox);
+        answerBox.appendChild(newItem);
 
-        letter.innerHTML = inputParse[i].toUpperCase();
     }
 }
 
